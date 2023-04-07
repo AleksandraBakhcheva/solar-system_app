24206ec3c88e271d0f2b51068ddfe202ed1a1963
Aleksandra <alexandra.bakhcheva@gmail.com>
2023-04-07 20:37:15 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./assets/styles/General.scss";
+import "./assets/styles/Variables.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
