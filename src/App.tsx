1215ae5675ec63e787adc73f84a7917c8b9e526c
Anna <momannuta@gmail.com>
2023-04-09 16:25:21 +0300

    add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./assets/styles/General.scss";
 import "./assets/styles/Variables.scss";
+import HeadTitle from "./assets/components/HeadTitle/HeadTitle";
 
 function App() {
   return (
     <div className="App">
       <p>Hello World!</p>
+      <HeadTitle/>
     </div>
   );
 }
