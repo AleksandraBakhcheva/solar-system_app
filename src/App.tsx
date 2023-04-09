1400f0b27724e4b6f9f9339c6ef1711b4dc45b1b
Anna <momannuta@gmail.com>
2023-04-09 16:45:04 +0300

    All planets gallery component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import AllPlanets from "./assets/components/AllPlanets/AllPlanets";
 import "./assets/styles/General.scss";
 import "./assets/styles/Variables.scss";
 
 function App() {
   return (
+    <>
     <div className="App">
       <p>Hello World!</p>
     </div>
+    <AllPlanets/>
+    </>
   );
 }
 
