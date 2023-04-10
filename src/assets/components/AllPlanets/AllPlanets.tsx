886632b0b71b732252fd3adccfcdfbec1fa9a4cc
Anna <momannuta@gmail.com>
2023-04-10 16:03:24 +0300

    fetch function

--- src/assets/components/AllPlanets/AllPlanets.tsx
+++ src/assets/components/AllPlanets/AllPlanets.tsx
@@ -1,33 +1,41 @@
+import { useEffect, useState } from "react";
 import "./AllPlanets.scss";
 import PlanetCard from "./PlanetCard";
 import { IPlanet } from "./interfaces";
 
-
-const planets: IPlanet[] = [
-    {
-        id: 1,
-        name: 'Earth',
-        img: 'https://s0.rbk.ru/v6_top_pics/resized/640xH/media/img/1/42/756629959272421.jpg'
-    },
-    {
-        id: 2,
-        name: 'Mars',
-        img: 'https://s0.rbk.ru/v6_top_pics/resized/640xH/media/img/1/42/756629959272421.jpg'
-    }
-]
+const needNames:string[] = ['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto']
 
 export default function AllPlanets(){
+    const [planets, setPlanets] = useState<IPlanet[]>([])
+
+    async function sendApiRequest(): Promise<IPlanet[]> {
+              //let response = await fetch(`https://api.le-systeme-solaire.net/rest/bodies/terre`)
+            return await fetch(`https://api.le-systeme-solaire.net/rest/bodies`)
+                .then(res => res.json())
+                .then(res => {
+                    const planetList: IPlanet[] = res.bodies;                    
+                    const needList: IPlanet[] = planetList.filter((planet) => needNames.includes(planet.englishName))
+                    console.log("needList", needList)                    
+                    setPlanets(needList)
+                    return needList
+                })
+    }
+
+
+    useEffect(() => {
+        sendApiRequest()
+    }, [])
+
     return(
         <div className='all-planets-container'>
-            <div className='card-wrap'>
-                <PlanetCard planet={planets[0]}/>
-                <PlanetCard planet={planets[1]}/>
-            </div>
+            {planets.map(planet => <PlanetCard planet={planet} key={planet.id}/>)}            
         </div>
     )
 
 }
 /**
+ *   <PlanetCard planet={planets[0]}/>
+                <PlanetCard planet={planets[1]}/>
  *  <div className='container'><h1 className='caption'>Flachcards</h1>
             <div className='card-wrap'>             
             {
--- src/assets/components/AllPlanets/PlanetCard.tsx
+++ src/assets/components/AllPlanets/PlanetCard.tsx
@@ -1,16 +1,29 @@
+//import { useState } from "react";
 import "./PlanetCard.scss";
 import { IPlanet } from "./interfaces";
+import Earth from '../../images/Earth.svg';
+import Jupiter from '../../images/Jupiter.svg';
+import Mars from '../../images/Mars.svg';
+import Mercury from '../../images/Mercury.svg';
+import Neptune from '../../images/Neptune.svg';
+import Pluto from '../../images/Pluto.svg';
+import Saturn from '../../images/Saturn.svg';
+import Uranus from '../../images/Uranus.svg';
+import venus from '../../images/venus.svg';
 
 interface PlanetProps{
     planet: IPlanet
 }
 
-export default function PlanetCard(props:PlanetProps){
+export default function PlanetCard({planet}:PlanetProps){
+    //const [item, setItem] = useState(planet.id)
+    const imagePath = `../../images/${planet.englishName}.svg`
     return(
-        <div className="card-wrap">
-            <img src={props.planet.img}/>
-            <div>{props.planet.name}</div>
+        <div className="card-wrap" > 
+            <img src={imagePath} alt={planet.englishName}/>
+            <div>{planet.englishName}</div>
         </div>
     )
 
 }
+//onClick={() => setItem(planet.id)}> <img src={planet.img} alt={planet.name}/>
