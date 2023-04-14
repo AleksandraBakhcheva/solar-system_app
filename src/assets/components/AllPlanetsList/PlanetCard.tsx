b640f3faa2c710d9ed7bf67825020b60899a1ce9
Anna <momannuta@gmail.com>
2023-04-14 18:29:55 +0300

    add focus

--- src/assets/components/AllPlanetsList/PlanetCard.tsx
+++ src/assets/components/AllPlanetsList/PlanetCard.tsx
@@ -1,17 +1,25 @@
 import "./AllPlanetsList.scss";
-import { useContext } from "react";
-import { NavLink } from "react-router-dom";
+import { useContext, useEffect, useRef } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import { IPlanet, PlanetProps } from "./Interfaces";
 import { PlanetsContext } from "../../сontext/PlanetsContext";
 
 export default function PlanetCard({planet}:PlanetProps){
     
-    const { images, planets } = useContext(PlanetsContext)
-    let imagePath:string = images[planet.englishName]
+  const { images, planets, } = useContext(PlanetsContext)
+  let imagePath: string = images[planet.englishName]
+  const cardRef = useRef<HTMLAnchorElement>(null);  
+  let location = useLocation();
+  
+  useEffect(() => {
+    if (cardRef.current?.className.includes('active')){
+      cardRef.current?.focus()
+    }
+  },[location.pathname]);
 
   return (
-    <div className="card" id={planet.englishName}>
-      <NavLink to={`/${planet.englishName}`}>
+    <div className="card" id={planet.englishName} >
+      <NavLink to={`/${planet.englishName}`} ref={cardRef}>
         <img className="card__img" src={imagePath} alt="planet" />
         <div className="card__name">{planet.englishName}</div>
       </NavLink>
