36b57c65799a9f7dde84c4ce31ea1bda3dae35d0
Anna <momannuta@gmail.com>
2023-04-18 22:16:55 +0300

    change planets images

--- src/assets/сontext/PlanetsContext.tsx
+++ src/assets/сontext/PlanetsContext.tsx
@@ -1,25 +1,15 @@
 import { createContext, useEffect, useState } from "react";
 import { IPlanet } from "../components/AllPlanetsList/Interfaces";
-import Earth from "../images/real-png/earth.png";
-import Jupiter from "../images/real-png/jupiter.png";
-import Mars from "../images/real-png/mars.png";
-import Mercury from "../images/real-png/mercury.png";
-import Neptune from "../images/real-png/neptune.png";
-import Pluto from "../images/real-png/pluto.png";
-import Saturn from "../images/real-png/saturn.png";
-import Uranus from "../images/real-png/uranus.png";
-import Venus from "../images/real-png/venus.png";
+import Earth from "../images/earth.png";
+import Jupiter from "../images/jupiter.png";
+import Mars from "../images/mars.png";
+import Mercury from "../images/mercury.png";
+import Neptune from "../images/neptune.png";
+import Pluto from "../images/pluto.png";
+import Saturn from "../images/saturn.png";
+import Uranus from "../images/uranus.png";
+import Venus from "../images/venus.png";
 import Error from "../components/Error/Error";
-
-// import Earth from "../images/cartoon-png/earth.png";
-// import Jupiter from "../images/cartoon-png/jupiter.png";
-// import Mars from "../images/cartoon-png/mars.png";
-// import Mercury from "../images/cartoon-png/mercury.png";
-// import Neptune from "../images/cartoon-png/neptune.png";
-// import Pluto from "../images/cartoon-png/pluto.png";
-// import Saturn from "../images/cartoon-png/saturn.png";
-// import Uranus from "../images/cartoon-png/uranus.png";
-// import Venus from "../images/cartoon-png/venus.png";
 
 interface IPlanetContext {
   setPlanets: any;
