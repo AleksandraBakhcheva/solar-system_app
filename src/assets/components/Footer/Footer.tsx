9e26b9c61ceade3603a4b600d739d5dfa65b17e8
Aleksandra <alexandra.bakhcheva@gmail.com>
2023-04-28 19:03:36 +0500

    press footer to the bottom, delete github.svg, modify footer

--- src/assets/components/Footer/Footer.tsx
+++ src/assets/components/Footer/Footer.tsx
@@ -1,17 +1,17 @@
 import "./Footer.scss";
-import github from "../../images/github.svg";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__container">
-        <div className="footer__container_link">
-          <a href="https://github.com/AleksandraBakhcheva/solar-system_app">
-            <img src={github} alt="github" />
-          </a>
-        </div>
-        <div>&#169; 2023</div>
-      </div>
+      <a
+        href="https://github.com/AleksandraBakhcheva/solar-system_app"
+        target="new__link"
+      >
+        <svg viewBox="0 0 24 24" aria-hidden="true" fill="#eee0ee" width="25">
+          <path d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 6.8 9.5c.5 0 .7-.2.7-.5v-1.7C6.7 20 6.1 18 6.1 18c-.4-1.2-1-1.5-1-1.5-1-.6 0-.6 0-.6 1 0 1.5 1 1.5 1 .9 1.6 2.4 1.1 3 .9 0-.7.3-1.1.6-1.4-2.2-.2-4.6-1-4.6-4.9 0-1.1.4-2 1-2.7 0-.3-.4-1.3.2-2.7 0 0 .8-.2 2.7 1a9.4 9.4 0 0 1 5 0c2-1.2 2.8-1 2.8-1 .5 1.4.1 2.4 0 2.7.7.7 1 1.6 1 2.7 0 3.8-2.3 4.7-4.5 5 .4.2.7.8.7 1.8V21c0 .3.2.6.7.5 4-1.3 6.8-5 6.8-9.5A10 10 0 0 0 12 2z"></path>
+        </svg>
+      </a>
+      <span>&#169; {new Date().getFullYear()}</span>
     </footer>
   );
 }
