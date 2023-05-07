28727d78ada39ebe46a052a2cb34a3c88c31fe5e
Aleksandra <alexandra.bakhcheva@gmail.com>
2023-05-07 22:33:26 +0500

    change layout + app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
 import "./assets/styles/General.scss";
-import Header from "./assets/components/Header/Header";
-import Footer from "./assets/components/Footer/Footer";
-import HeadTitle from "./assets/components/HeadTitle/HeadTitle";
-import AllPlanets from "./assets/components/AllPlanetsList/AllPlanetsList";
-import Layout from "./assets/components/Layout/Layout";
-import Statistics from "./assets/components/Statistics/Statistics";
-import PlanetInfo from "./assets/components/PlanetInfo/PlanetInfo";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { PlanetsContextProvider } from "./assets/сontext/PlanetsContext";
+import Layout from "./assets/components/Layout/Layout";
+import PlanetInfo from "./assets/components/PlanetInfo/PlanetInfo";
 
 function App() {
   return (
     <PlanetsContextProvider>
-      <Header />
-      <main>
-        <HeadTitle />
-        <AllPlanets />
+      <>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="/" element={<Navigate replace to="/Earth" />} />
             <Route path="/:id" element={<PlanetInfo />} />
           </Route>
         </Routes>
-        <Statistics />
-      </main>
-      <Footer />
+      </>
     </PlanetsContextProvider>
   );
 }
--- src/assets/components/Layout/Layout.tsx
+++ src/assets/components/Layout/Layout.tsx
@@ -1,9 +1,21 @@
 import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+import HeadTitle from "../HeadTitle/HeadTitle";
+import AllPlanets from "../AllPlanetsList/AllPlanetsList";
+import Statistics from "../Statistics/Statistics";
+import Footer from "../Footer/Footer";
 
 export default function Layout() {
   return (
-    <>    
-      <Outlet />
-    </> 
+    <>
+      <Header />
+      <main>
+        <HeadTitle />
+        <AllPlanets />
+        <Outlet />
+        <Statistics />
+      </main>
+      <Footer />
+    </>
   );
 }
